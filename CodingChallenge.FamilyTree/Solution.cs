aa4c2293eb5fff0df8229b0949a8970b43546335
Ben Greaves <bsgreaves@gmail.com>
2018-10-02 22:37:06 -0500

    All tests pass

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace CodingChallenge.FamilyTree
 {
     public class Solution
     {
+        // so there's a couple of ways to go about this one
+        // but considering the relative simplicity of the family tree 
+        //  (no marriages/couples so no more than one parent,
+        //   entry to the tree from the top as opposed to entry from the middle or bottom,
+        //   etc.)
+        // probably the most straightforward method is to use recursion
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            if (descendantName == person.Name) return person.Birthday.ToString("MMMM");
+            if (!(bool)person.Descendants?.Any()) return string.Empty;
+            foreach (var descendant in person.Descendants)
+            {
+                var resultFromAllDescendents = GetBirthMonth(descendant, descendantName);
+                if (!string.IsNullOrWhiteSpace(resultFromAllDescendents)) return resultFromAllDescendents;
+            }
+            return string.Empty;
         }
     }
 }
--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -5,9 +5,47 @@ namespace CodingChallenge.PirateSpeak
 {
     public class Solution
     {
+        // *** Note: I don't normally leave comments. 
+        // In my opinion, clean code should be self documenting whenever possible.
+        // These comments are for illustrating my thought patterns.
+
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            // when you can, scrub your inputs
+            if (!IsValidInput(jumble) ||
+                !InputHasValidDictionaryMatch(jumble, dictionary))
+                return null;
+            
+            // basically, we're looking for anagrams
+            // we can reason that any 2 words can be alphebetised and compared to see if they are anagrams
+            var alphabetisedInput = GetAlphabetisedString(jumble);
+            return dictionary.Where(word => string.Equals(GetAlphabetisedString(word), alphabetisedInput))
+                             .ToArray();
+        }
+
+        // Is this function necessary? Probably not.
+        // But it gives us a nice SRP unit-of-work,
+        // and if we ever add more validation 
+        // (pirate utterances can't contain numbers or punctuation, etc.)
+        // then future devs know exactly where to place those new rules.
+        bool IsValidInput(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
+        }
+
+        // Why use this function? The name is Self-documenting. 
+        // Devs can skim the function name without trying to parse line after line of linq
+        bool InputHasValidDictionaryMatch(string input, string[] dictionary)
+        {
+            return dictionary.Select(word => word.Length)
+                             .Any(wordLength => wordLength == input.Length);
+        }
+
+        string GetAlphabetisedString(string input)
+        {
+            var inputChars = input.ToCharArray();
+            Array.Sort<char>(inputChars);
+            return new string(inputChars);
         }
     }
 }
